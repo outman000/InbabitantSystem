748c97f0100c481c1bd8ea327ce9658de663760f
柴健 <1054254369@qq.com>
2020-05-20 13:43:54 +0800

    新增字段 2020-05-20

---
+++ Dtol/Migrations/20200520022855_hjxx.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dtol.Migrations
+{
+    public partial class hjxx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "houseInfo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "houseInfo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "County",
+                table: "houseInfo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "houseInfo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Register",
+                table: "houseInfo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "houseInfo");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "houseInfo");
+
+            migrationBuilder.DropColumn(
+                name: "County",
+                table: "houseInfo");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "houseInfo");
+
+            migrationBuilder.DropColumn(
+                name: "Register",
+                table: "houseInfo");
+        }
+    }
+}
--- ViewModel/InhabitantSystem/MiddleViewModel/HouseInfoAddMiddle.cs
+++ ViewModel/InhabitantSystem/MiddleViewModel/HouseInfoAddMiddle.cs
@@ -18,6 +18,29 @@ namespace ViewModel.InhabitantSystem.MiddleViewModel
         /// 户主身份证号
         /// </summary>
         public string HouseHolderIdNo { get; set; }
+
+        /// <summary>
+        /// 户籍地
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 省
+        /// </summary>
+        public string Province { get; set; }
+        /// <summary>
+        /// 市
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// 县
+        /// </summary>
+        public string County { get; set; }
+
+        /// <summary>
+        /// 户籍状况
+        /// </summary>
+        public string Register { get; set; }
+
         /// <summary>
         /// 小区名称
         /// </summary>
